22875df2e61bde99bb1e1be893ef13ec1e300e63
xuxin <827556535>
2021-03-09 18:19:33 +0800

    add:添加生成二维码项目

---
+++ src/UsefullTinyTools/GenerateQRCode/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GenerateQRCode
+{
+    public partial class QrCode : Form
+    {
+        public QrCode()
+        {
+            InitializeComponent();
+        }
+
+        private void QrCode_Load( object sender, EventArgs e )
+        {
+            cb_version.SelectedIndex = 1;
+            cb_pixel.SelectedIndex = 0;
+            cb_icon_size.SelectedIndex = 0;
+            cb_icon_size.SelectedIndex = 1;
+        }
+
+        private void bt_generate_Click( object sender, EventArgs e )
+        {
+            var version = Convert.ToInt16(cb_version.Text);
+
+            var pixel = Convert.ToInt16(cb_pixel.Text);
+
+            string str_msg = tb_msg.Text;
+
+            int int_icon_size = Convert.ToInt16(cb_icon_size.Text);
+
+            int int_icon_border = Convert.ToInt16(cb_icon_size.Text);
+
+            bool b_we = rb_we_y.Checked ? true : false;
+
+            var imgIcon = ImageHelper.UrlToImage("");
+            Bitmap bmpIcon = new Bitmap(imgIcon);
+
+            var bmp = QRCodeHelper.GenerateCode(str_msg, version, pixel, bmpIcon, int_icon_size, int_icon_border, b_we);
+
+            pb_qrcode.Image = bmp;
+        }
+    }
+}
---
+++ src/UsefullTinyTools/GenerateQRCode/QRCodeHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using QRCoder;
+
+namespace GenerateQRCode
+{
+    public static class QRCodeHelper
+    {
+        /// <summary>
+        /// 生成二维码
+        /// </summary>
+        /// <param name="msg">二维码内容</param>
+        /// <param name="version">版本</param>
+        /// <param name="pixel">像素</param>
+        /// <param name="icon_path">图标地址</param>
+        /// <param name="icon_size">图标大小</param>
+        /// <param name="icon_border">图标边界</param>
+        /// <param name="white_edge">二维码白边</param>
+        /// <returns></returns>
+        public static Bitmap GenerateCode( string msg, int version, int pixel, string icon_path, int icon_size, int icon_border, bool white_edge )
+        {
+            using var code_generator = new QRCodeGenerator();
+            var code_data = code_generator.CreateQrCode(msg, QRCodeGenerator.ECCLevel.M, true, true, QRCodeGenerator.EciMode.Utf8, version);
+            var code = new QRCode(code_data);
+            var icon = new Bitmap(icon_path);
+            var bmp = code.GetGraphic(pixel, Color.Black, Color.White, icon, icon_size, icon_border, white_edge);
+            return bmp;
+
+        }
+
+        /// <summary>
+        /// 生成二维码
+        /// </summary>
+        /// <param name="msg">二维码内容</param>
+        /// <param name="version">版本</param>
+        /// <param name="pixel">像素</param>
+        /// <param name="icon_path">图标地址</param>
+        /// <param name="icon_size">图标大小</param>
+        /// <param name="icon_border">图标边界</param>
+        /// <param name="white_edge">二维码白边</param>
+        /// <returns></returns>
+        public static Bitmap GenerateCode( string msg, int version, int pixel, Bitmap icon, int icon_size, int icon_border, bool white_edge )
+        {
+            using var code_generator = new QRCodeGenerator();
+            var code_data = code_generator.CreateQrCode(msg, QRCodeGenerator.ECCLevel.M, true, true, QRCodeGenerator.EciMode.Utf8, version);
+            var code = new QRCode(code_data);
+
+            var bmp = code.GetGraphic(pixel, Color.Black, Color.White, icon, icon_size, icon_border, white_edge);
+            return bmp;
+        }
+    }
+}
