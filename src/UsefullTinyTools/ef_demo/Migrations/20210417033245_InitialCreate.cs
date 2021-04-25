117c02c0c68aba095c54541c02766c158d6f330e
xuxin <827556535@qq.com>
2021-04-25 23:07:14 +0800

    111

---
+++ src/UsefullTinyTools/ef_demo/Migrations/20210417033245_InitialCreate.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ef_demo.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ModelAs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModelAs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ModelBs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    ModelAId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ModelBs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ModelBs_ModelAs_ModelAId",
+                        column: x => x.ModelAId,
+                        principalTable: "ModelAs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ModelBs_ModelAId",
+                table: "ModelBs",
+                column: "ModelAId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ModelBs");
+
+            migrationBuilder.DropTable(
+                name: "ModelAs");
+        }
+    }
+}
---
+++ src/UsefullTinyTools/ef_demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ef_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new BaseDbContext();
+            //添加
+            context.Add(new ModelA { Id = 10, Name = "测试" });
+            context.SaveChanges();//保存数据到数据库中
+                                  //查询
+            var modelA = context.ModelAs.Where(p => p.Id > 1).First();
+            //更新
+            modelA.Name += DateTime.Now;
+            context.SaveChanges();
+            //删除
+            context.Remove(modelA);
+            context.SaveChanges();
+            context.Dispose();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
