2255fbde2658d7626bf840133187bd93d0e48457
xuxin <827556535>
2021-06-30 09:47:49 +0800

    add:单例

---
+++ src/UsefullTinyTools/SingletonDemo/SingletonTestEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace SingletonDemo
+{
+    public static class SingletonTestEnum
+    {
+        public static string Statistest()
+        {
+            return string.Empty;
+        }
+    }
+
+}
